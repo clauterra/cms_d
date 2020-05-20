4bd3bdd2e58f49b607d6f1a545a2c4687bf5ba07
clauterra <38335060+clauterra@users.noreply.github.com>
2020-05-20 10:49:46 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const app = function () {
-	const API_BASE = 'https://script.google.com/macros/s/AKfycbyP5Rifn7Q05Qcd7CTfm-AOouFHHvUAvCVVuKSfQu-LCqJocP8/exec';
+	const API_BASE = 'https://script.google.com/macros/s/AKfycbzjJBb0IIwb_jlkCsWSHkb3zWNUrQ-CBtRZJT66FA/exec';
 	const API_KEY = 'abcdef';
 	const CATEGORIES = ['general', 'financial', 'technology', 'marketing'];
 
